5030dc1a3e6351b0d499a13459ec517cba0c7bd7
vladrus13 <vladrus13rus@yandex.ru>
2021-04-08 12:54:32 +0300

    Add Empty Frame

---
+++ src/main/java/ru/vladrus13/jgraphic/basic/components/EmptyFrame.java
@@ -0,0 +1,41 @@
+package ru.vladrus13.jgraphic.basic.components;
+
+import ru.vladrus13.graphic.Graphics;
+import ru.vladrus13.jgraphic.basic.Frame;
+import ru.vladrus13.jgraphic.bean.Point;
+import ru.vladrus13.jgraphic.bean.Size;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+
+public class EmptyFrame extends Frame {
+
+    public EmptyFrame(String name, Frame parent) {
+        super(name, parent);
+    }
+
+    public EmptyFrame(String name, Point start, Size size, Frame parent) {
+        super(name, start, size, parent);
+    }
+
+    @Override
+    protected void nonCheckingDraw(Graphics graphics) {
+        for (Frame child : childes) {
+            child.draw(graphics);
+        }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (!focused.isEmpty()) {
+            focused.getFirst().keyPressed(e);
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        if (!focused.isEmpty()) {
+            focused.getFirst().mousePressed(e);
+        }
+    }
+}
